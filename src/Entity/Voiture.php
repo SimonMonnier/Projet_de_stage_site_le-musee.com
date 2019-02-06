548857be55d8b6e60f07dc2d8ff49870abbe3142
smonnier44 <s.monnier44440@gmail.com>
2019-02-06 14:45:47 +0100

    mise à jour

---
+++ src/Entity/Voiture.php
@@ -0,0 +1,466 @@
+<?php
+namespace App\Entity;
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\VoitureRepository")
+ */
+class Voiture
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $marque;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $introduction;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $contenu;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $coverImage;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $modèle;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $année;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createAt;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $numéroSérie;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $longueur;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $largeur;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $hauteur;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $poidsAVide;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $carburant;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $kilomètrage;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $couleurCarrosserie;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $couleurIntérieur;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $puissance;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $origine;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $boiteDeVitesse;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $moteurEtCylindrée;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $CvFiscaux;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $conduite;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $nombreDePlace;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $carrosserie;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $état;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMarque(): ?string
+    {
+        return $this->marque;
+    }
+
+    public function setMarque(string $marque): self
+    {
+        $this->marque = $marque;
+
+        return $this;
+    }
+
+    public function getModèle(): ?string
+    {
+        return $this->modèle;
+    }
+
+    public function setModèle(string $modèle): self
+    {
+        $this->modèle = $modèle;
+
+        return $this;
+    }
+
+    public function getAnnée(): ?int
+    {
+        return $this->année;
+    }
+
+    public function setAnnée(int $année): self
+    {
+        $this->année = $année;
+
+        return $this;
+    }
+
+    public function getCreateAt(): ?\DateTimeInterface
+    {
+        return $this->createAt;
+    }
+
+    public function setCreateAt(\DateTimeInterface $createAt): self
+    {
+        $this->createAt = $createAt;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(?string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getNuméroSérie(): ?string
+    {
+        return $this->numéroSérie;
+    }
+
+    public function setNuméroSérie(?string $numéroSérie): self
+    {
+        $this->numéroSérie = $numéroSérie;
+
+        return $this;
+    }
+
+    public function getLongueur(): ?string
+    {
+        return $this->longueur;
+    }
+
+    public function setLongueur(?string $longueur): self
+    {
+        $this->longueur = $longueur;
+
+        return $this;
+    }
+
+    public function getLargeur(): ?string
+    {
+        return $this->largeur;
+    }
+
+    public function setLargeur(?string $largeur): self
+    {
+        $this->largeur = $largeur;
+
+        return $this;
+    }
+
+    public function getHauteur(): ?string
+    {
+        return $this->hauteur;
+    }
+
+    public function setHauteur(?string $hauteur): self
+    {
+        $this->hauteur = $hauteur;
+
+        return $this;
+    }
+
+    public function getPoidsAVide(): ?string
+    {
+        return $this->poidsAVide;
+    }
+
+    public function setPoidsAVide(?string $poidsAVide): self
+    {
+        $this->poidsAVide = $poidsAVide;
+
+        return $this;
+    }
+
+    public function getCarburant(): ?string
+    {
+        return $this->carburant;
+    }
+
+    public function setCarburant(string $carburant): self
+    {
+        $this->carburant = $carburant;
+
+        return $this;
+    }
+
+    public function getKilomètrage(): ?string
+    {
+        return $this->kilomètrage;
+    }
+
+    public function setKilomètrage(?string $kilomètrage): self
+    {
+        $this->kilomètrage = $kilomètrage;
+
+        return $this;
+    }
+
+    public function getCouleurCarrosserie(): ?string
+    {
+        return $this->couleurCarrosserie;
+    }
+
+    public function setCouleurCarrosserie(?string $couleurCarrosserie): self
+    {
+        $this->couleurCarrosserie = $couleurCarrosserie;
+
+        return $this;
+    }
+
+    public function getCouleurIntérieur(): ?string
+    {
+        return $this->couleurIntérieur;
+    }
+
+    public function setCouleurIntérieur(?string $couleurIntérieur): self
+    {
+        $this->couleurIntérieur = $couleurIntérieur;
+
+        return $this;
+    }
+
+    public function getPuissance(): ?string
+    {
+        return $this->puissance;
+    }
+
+    public function setPuissance(?string $puissance): self
+    {
+        $this->puissance = $puissance;
+
+        return $this;
+    }
+
+    public function getOrigine(): ?string
+    {
+        return $this->origine;
+    }
+
+    public function setOrigine(?string $origine): self
+    {
+        $this->origine = $origine;
+
+        return $this;
+    }
+
+    public function getBoiteDeVitesse(): ?string
+    {
+        return $this->boiteDeVitesse;
+    }
+
+    public function setBoiteDeVitesse(?string $boiteDeVitesse): self
+    {
+        $this->boiteDeVitesse = $boiteDeVitesse;
+
+        return $this;
+    }
+
+    public function getMoteurEtCylindrée(): ?string
+    {
+        return $this->moteurEtCylindrée;
+    }
+
+    public function setMoteurEtCylindrée(?string $moteurEtCylindrée): self
+    {
+        $this->moteurEtCylindrée = $moteurEtCylindrée;
+
+        return $this;
+    }
+
+    public function getCvFiscaux(): ?string
+    {
+        return $this->CvFiscaux;
+    }
+
+    public function setCvFiscaux(?string $CvFiscaux): self
+    {
+        $this->CvFiscaux = $CvFiscaux;
+
+        return $this;
+    }
+
+    public function getConduite(): ?string
+    {
+        return $this->conduite;
+    }
+
+    public function setConduite(?string $conduite): self
+    {
+        $this->conduite = $conduite;
+
+        return $this;
+    }
+
+    public function getNombreDePlace(): ?string
+    {
+        return $this->nombreDePlace;
+    }
+
+    public function setNombreDePlace(?string $nombreDePlace): self
+    {
+        $this->nombreDePlace = $nombreDePlace;
+
+        return $this;
+    }
+
+    public function getCarrosserie(): ?string
+    {
+        return $this->carrosserie;
+    }
+
+    public function setCarrosserie(?string $carrosserie): self
+    {
+        $this->carrosserie = $carrosserie;
+
+        return $this;
+    }
+
+    public function getétat(): ?string
+    {
+        return $this->état;
+    }
+
+    public function setétat(?string $état): self
+    {
+        $this->état = $état;
+
+        return $this;
+    }
+
+    public function getIntroduction(): ?string
+    {
+        return $this->introduction;
+    }
+
+    public function setIntroduction(?string $carrosserie): self
+    {
+        $this->introduction = $introduction;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(?string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCoverImage(): ?string
+    {
+        return $this->coverImage;
+    }
+
+    public function setCoverImage(?string $coverImage): self
+    {
+        $this->coverImage = $coverImage;
+
+        return $this;
+    }
+}
---
+++ src/Migrations/Version20190206133357.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190206133357 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE voiture (id INT AUTO_INCREMENT NOT NULL, marque VARCHAR(255) NOT NULL, introduction LONGTEXT NOT NULL, contenu LONGTEXT NOT NULL, cover_image VARCHAR(255) NOT NULL, modèle VARCHAR(255) NOT NULL, année INT NOT NULL, create_at DATETIME NOT NULL, type VARCHAR(255) DEFAULT NULL, numéro_série VARCHAR(255) DEFAULT NULL, longueur VARCHAR(255) DEFAULT NULL, largeur VARCHAR(255) DEFAULT NULL, hauteur VARCHAR(255) DEFAULT NULL, poids_avide VARCHAR(255) DEFAULT NULL, carburant VARCHAR(255) NOT NULL, kilomètrage VARCHAR(255) DEFAULT NULL, couleur_carrosserie VARCHAR(255) DEFAULT NULL, couleur_intérieur VARCHAR(255) DEFAULT NULL, puissance VARCHAR(255) DEFAULT NULL, origine VARCHAR(255) DEFAULT NULL, boite_de_vitesse VARCHAR(255) DEFAULT NULL, moteur_et_cylindrée VARCHAR(255) DEFAULT NULL, cv_fiscaux VARCHAR(255) DEFAULT NULL, conduite VARCHAR(255) DEFAULT NULL, nombre_de_place VARCHAR(255) DEFAULT NULL, carrosserie VARCHAR(255) DEFAULT NULL, état VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE voiture');
+    }
+}
