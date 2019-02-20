112bc5cbb019c2512cf60012e324f3bed63ca31d
Joachim Sall <joachimsall8@gmail.com>
2019-02-20 16:07:08 +0100

    maj

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -2,18 +2,36 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Articles;
+use App\Repository\ArticlesRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ArticlesController extends AbstractController
 {
     /**
-     * @Route("/articles", name="articles")
+     * @Route("/actualités", name="articles")
      */
-    public function index()
+    public function index(ArticlesRepository $repoArticles)
     {
+        $articles = $repoArticles->findAll();
+
         return $this->render('articles/index.html.twig', [
-            'controller_name' => 'ArticlesController',
+            'articles' => $articles,
+        ]);
+    }
+
+    /**
+     * Permet d'afficher le descriptif complet d'une voiture
+     *
+     * @Route("/articles/{slug}", name="articles_show")
+     * 
+     * @return Response
+     */
+    public function show(Articles $article)
+    {
+        return $this->render('articles/show.html.twig', [
+            'article' => $article
         ]);
     }
 }
