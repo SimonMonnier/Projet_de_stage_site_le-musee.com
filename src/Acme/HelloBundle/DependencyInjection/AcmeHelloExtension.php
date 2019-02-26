f2c4b2751278de131a50c7ae7063b14bd97aa492
John <guerard.john@gmail.com>
2019-02-26 11:19:39 +0100

    ok

---
+++ src/Acme/HelloBundle/DependencyInjection/AcmeHelloExtension.php
@@ -0,0 +1,12 @@
+namespace Acme\HelloBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class AcmeHelloExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        // ... you'll load the files here later
+    }
+}
