39902e0f6610c9ed01fa6ef3d2b8b9ccb6f84666
Joachim Sall <joachimsall8@gmail.com>
2019-02-26 17:52:09 +0100

    maj

---
+++ src/Form/ContactType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+
+class ContactType extends AbstractType
+{
+    /**
+     * Permet d'avoir la configuration de base d'un champ
+     *
+     * @param string $label
+     * @param string $placeholder
+     * @param array $options
+     * @return array
+     */
+    private function getConfiguration($label, $placeholder, $options = [])
+    {
+        return array_merge([
+            'label' => $label,
+            'attr' => [
+                'placeholder' => $placeholder
+            ]
+        ], $options);
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom', TextType::class, $this->getConfiguration("Nom: ", ""))
+            ->add('courriel', EmailType::class, $this->getConfiguration("Courriel: ", ""))
+            ->add('telephone', TextType::class, $this->getConfiguration("Téléphone: ", ""))
+            ->add('objet', TextType::class, $this->getConfiguration("Objet du message: ", ""))
+            ->add('message', TextareaType::class, $this->getConfiguration("Message: ", ""))
+            ->add('Enregistrer', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-success']
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
